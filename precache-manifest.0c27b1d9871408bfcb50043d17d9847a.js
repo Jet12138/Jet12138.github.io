0e977b8a62f2224e3f39418334a46d6fa004bcff
Jet12138 <390715001@qq.com>
2020-06-10 14:26:49 +0800

    尝试用create-react-app build后的打包文件夹来部署

---
+++ precache-manifest.0c27b1d9871408bfcb50043d17d9847a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1fc2f2626048d927d9d8",
+    "url": "/static/css/main.41ee67ef.chunk.css"
+  },
+  {
+    "revision": "1fc2f2626048d927d9d8",
+    "url": "/static/js/main.1fc2f262.chunk.js"
+  },
+  {
+    "revision": "73d74e1cceaaed8d0fc5",
+    "url": "/static/js/1.73d74e1c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "6f9ffa986999d9fb9afae5fc9195eed0",
+    "url": "/index.html"
+  }
+];
